27afd3188da7a8af864e99f80f1123378e9502c7
bunin-av <bunin.adv@gmail.com>
2021-10-18 23:19:50 +0300

    hw-07 / oop-02.js

--- oop/oop-02.js
+++ oop/oop-02.js
@@ -2,8 +2,9 @@ class SyncPromise {
   stack = []
 
   constructor(executor) {
-    let resolve
-    let reject
+    let
+      resolve,
+      reject
 
     try {
       this.state = 'pending'
@@ -11,13 +12,17 @@ class SyncPromise {
 
       resolve = (val) => {
         if (this.state !== 'pending') return
-        this.state = 'fulfilled'
-        if (this.stack.length > 0) {
-          const cb = this.stack.pop()
-          val.then(cb)
-          return
+
+        if (val && val.then) {
+          val.then(v => {
+            const cb = this.stack[0]
+            this.value = val
+            cb(v)
+          })
+        } else {
+          this.state = 'fulfilled'
+          this.value = val
         }
-        this.value = val
       }
 
       reject = (val) => {
@@ -34,17 +39,16 @@ class SyncPromise {
 
   then(cb, cb2) {
     return new SyncPromise((res, rej) => {
-      if (this.state === 'fulfilled') {
-        if (typeof cb === 'function') {
-          if (this.value.then) {
-            this.stack.push(res)
-          } else {
-            res(cb(this.value))
-          }
-        }
+      if (this.state === 'fulfilled' && typeof cb === 'function') {
+        res(cb(this.value))
       }
+      else if (this.state === 'pending' && typeof cb === 'function') {
+        this.stack.push(
+          (v) => res(cb(v)),
+          (v) => rej(cb2(v))
+        )}
 
-      if (this.state === 'rejected' && !cb2) {
+      else if (this.state === 'rejected' && !cb2) {
         return new SyncPromise((_, rej) => rej(this.value))
       }
     })
@@ -53,6 +57,7 @@ class SyncPromise {
 
 console.log(1)
 let q = new SyncPromise((res) => res(3))
-// let q = new SyncPromise((res) => res(Promise.resolve(3)))
+let qq = new SyncPromise((res) => res(Promise.resolve(5)))
 console.log(2)
-q.then(console.log)
+q.then(console.log).then(console.log)
+qq.then(console.log).then(console.log)
